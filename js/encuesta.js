95925fab00d58b1aa5927447b4511b99d76966ec
tomas <toomiileon@gmail.com>
2024-10-21 18:08:02 -0300

    simplifique la solucion del calculo de moda para no utilizar objetos

--- js/encuesta.js
+++ js/encuesta.js
@@ -1,5 +1,6 @@
-
-const respuestas = [8, 5, 10, 7, 6, 8, 9, 10, 7, 4, 6, 3, 7, 8, 6, 5, 4, 2, 9, 10];
+const respuestas = [
+  8, 5, 10, 7, 6, 8, 9, 10, 7, 4, 6, 3, 7, 8, 6, 5, 4, 2, 9, 10,
+];
 
 // Función para calcular el promedio de satisfacción
 function calcularPromedio(respuestas) {
@@ -15,7 +16,7 @@ function contarSatisfechos(respuestas) {
   let clientesSatisfechos = 0;
   for (let i = 0; i < respuestas.length; i++) {
     if (respuestas[i] >= 7) {
-        clientesSatisfechos++;
+      clientesSatisfechos++;
     }
   }
   return clientesSatisfechos;
@@ -26,7 +27,7 @@ function contarInsatisfechos(respuestas) {
   let clientesInsatisfechos = 0;
   for (let i = 0; i < respuestas.length; i++) {
     if (respuestas[i] <= 4) {
-        clientesInsatisfechos++;
+      clientesInsatisfechos++;
     }
   }
   return clientesInsatisfechos;
@@ -34,19 +35,26 @@ function contarInsatisfechos(respuestas) {
 
 // Función para encontrar la moda (la calificación más frecuente)
 function calcularModa(respuestas) {
-  let frecuencia = {};
+
+  const frecuencia = [0,0,0,0,0,0,0,0,0,0,0]; // Índices 0 a 10, pero usaremos solo 1 a 10
+
+  // Contamos las frecuencias
+  for (let i = 0; i < respuestas.length; i++) {
+    let calificacion = respuestas[i];
+    frecuencia[calificacion]++; // Incrementamos la frecuencia
+  }
+
+  // Encontrar la moda
   let maxFrecuencia = 0;
   let moda = [];
 
-  for (let i = 0; i < respuestas.length; i++) {
-    let calificacion = respuestas[i];
-    frecuencia[calificacion] = (frecuencia[calificacion] || 0) + 1;
-
-    if (frecuencia[calificacion] > maxFrecuencia) {
-      maxFrecuencia = frecuencia[calificacion];
-      moda = [calificacion];  
-    } else if (frecuencia[calificacion] === maxFrecuencia) {
-      moda.push(calificacion); 
+  for (let i = 1; i <= 10; i++) {
+    // Solo iteramos de 1 a 10
+    if (frecuencia[i] > maxFrecuencia) {
+      maxFrecuencia = frecuencia[i];
+      moda = [i]; // Reiniciamos la moda con la nueva calificación
+    } else if (frecuencia[i] === maxFrecuencia) {
+      moda.push(i); // Añadimos la calificación a la moda
     }
   }
 
@@ -65,6 +73,14 @@ const moda = calcularModa(respuestas);
 document.write("<p>Promedio de satisfacción: " + promedio.toFixed(2) + "</p>");
 document.write("<p>Clientes satisfechos: " + satisfechos + "</p>");
 document.write("<p>Clientes insatisfechos: " + insatisfechos + "</p>");
-document.write("<p>Porcentaje de clientes satisfechos: " + porcentajeSatisfechos.toFixed(2) + "%</p>");
-document.write("<p>Porcentaje de clientes insatisfechos: " + porcentajeInsatisfechos.toFixed(2) + "%</p>");
+document.write(
+  "<p>Porcentaje de clientes satisfechos: " +
+    porcentajeSatisfechos.toFixed(2) +
+    "%</p>"
+);
+document.write(
+  "<p>Porcentaje de clientes insatisfechos: " +
+    porcentajeInsatisfechos.toFixed(2) +
+    "%</p>"
+);
 document.write("<p>La moda es: " + moda.join(", ") + "</p>");
